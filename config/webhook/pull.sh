cd713b5e595699378d74ff98ff5b44026a8a17b6
Michael Kreil <github@michael-kreil.de>
2024-02-15 21:12:22 +0100

    update frontend on webhook

--- config/webhook/pull.sh
+++ config/webhook/pull.sh
@@ -2,5 +2,6 @@
 set -x
 cd /var/www/tiles.versatiles.org/
 git pull -f
+wget -q "https://github.com/versatiles-org/versatiles-frontend/releases/latest/download/frontend.br.tar" -O /var/www/data/frontend.br.tar
 rm -r /var/www/ramdisk/*
 nginx -s reload
