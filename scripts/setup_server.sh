105c1fea8b961cc291f858c44c9f8937b7836823
Michael Kreil <github@michael-kreil.de>
2024-04-05 12:01:36 +0200

    fix: disable webhook

--- scripts/setup_server.sh
+++ scripts/setup_server.sh
@@ -10,10 +10,9 @@ echo -e "${RED}SETUP SYSTEM${NC}"
 curl -fsSL https://deb.nodesource.com/setup_lts.x | bash - # for node js
 apt-get -qq -y update
 apt-get -qq -y upgrade
-apt-get -qq -y install curl git libnginx-mod-http-brotli-filter libnginx-mod-http-brotli-static nginx-full supervisor ufw webhook
+apt-get -qq -y install curl git libnginx-mod-http-brotli-filter libnginx-mod-http-brotli-static nginx-full supervisor ufw 
 ufw allow OpenSSH
 ufw allow 8080/tcp
-ufw allow 9000/tcp
 ufw --force enable
 
 echo -e "${RED}ADD USER${NC}"
@@ -53,9 +52,9 @@ ln -s /var/www/tiles.versatiles.org/config/nginx/nginx.conf /etc/nginx/nginx.con
 ln -s /var/www/tiles.versatiles.org/config/nginx/tiles.versatiles.org.conf /etc/nginx/sites/tiles.versatiles.org.conf
 nginx -s reload
 
-echo -e "${RED}CONFIG WEBHOOK${NC}"
-ln -s /var/www/tiles.versatiles.org/config/supervisor/webhooks.conf /etc/supervisor/conf.d/webhooks.conf
-cat /var/www/tiles.versatiles.org/config/webhook/webhooks.yaml | sed "s/%SECRET%/$SECRET/g" > /var/www/webhooks.yaml
+#echo -e "${RED}CONFIG WEBHOOK${NC}"
+#ln -s /var/www/tiles.versatiles.org/config/supervisor/webhooks.conf /etc/supervisor/conf.d/webhooks.conf
+#cat /var/www/tiles.versatiles.org/config/webhook/webhooks.yaml | sed "s/%SECRET%/$SECRET/g" > /var/www/webhooks.yaml
 
 echo -e "${RED}CONFIG SUPERVISOR${NC}"
 rm /etc/supervisor/supervisord.conf
