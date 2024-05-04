98cb2b55b33f5633cc22772d4021fb82bbccc9d4
Michael Kreil <github@michael-kreil.de>
2024-05-04 21:09:44 +0200

    download hillshading

--- bin/data/update.sh
+++ bin/data/update.sh
@@ -11,22 +11,33 @@ source .env
 # Ensure the directory for Versatiles data exists.
 mkdir -p volumes/versatiles
 
-if [ ! -f volumes/versatiles/osm.versatiles ]; then
-    # Download OpenStreetMap data in Versatiles format.
-    if [ -z "$BBOX" ]; then
-        # Download the complete dataset if BBOX is not specified.
-        echo "Downloading the complete planet data..."
-        wget --progress=dot:giga "https://download.versatiles.org/osm.versatiles" -O volumes/versatiles/temp.versatiles
-        mv volumes/versatiles/temp.versatiles volumes/versatiles/osm.versatiles
-    else
-        # Download only the specified BBOX area.
-        echo "Downloading data for specified BBOX..."
-        mkdir -p volumes/temp
-        rm -rf volumes/temp/*
-        docker run -v "$(pwd)/volumes/temp/:/data/:rw" versatiles/versatiles:latest-scratch versatiles convert --bbox "$BBOX" --bbox-border 3 'https://download.versatiles.org/osm.versatiles' /data/osm.versatiles
-        mv volumes/temp/osm.versatiles volumes/versatiles/osm.versatiles
+mkdir -p volumes/temp
+rm -rf volumes/temp/*
+
+function download {
+    local URL=$1
+    local FILENAME=$2
+    
+    if [ ! -f volumes/versatiles/osm.versatiles ]; then
+        # Download OpenStreetMap data in Versatiles format.
+        if [ -z "$BBOX" ]; then
+            # Download the complete dataset if BBOX is not specified.
+            echo "Downloading the complete planet data..."
+            wget --progress=dot:giga "$URL" -O "volumes/versatiles/$FILENAME"
+            mv "volumes/versatiles/$FILENAME" "volumes/versatiles/$FILENAME"
+        else
+            # Download only the specified BBOX area.
+            echo "Downloading data for specified BBOX..."
+            mkdir -p volumes/temp
+            rm -rf volumes/temp/*
+            docker run -v "$(pwd)/volumes/temp/:/data/:rw" versatiles/versatiles:latest-scratch versatiles convert --bbox "$BBOX" --bbox-border 3 "$URL" "/data/$FILENAME"
+            mv "volumes/temp/$FILENAME" "volumes/versatiles/$FILENAME"
+        fi
     fi
-fi
+}
+
+download https://download.versatiles.org/osm.versatiles osm.versatiles
+download https://download.versatiles.org/hillshade-vectors.versatiles hillshade-vectors.versatiles
 
 # Check for successful download and setup.
 if [ ! -f volumes/versatiles/osm.versatiles ]; then
