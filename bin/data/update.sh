0388693423d70aa9f4dd15bc0b737a2be9ef3ad2
Michael Kreil <github@michael-kreil.de>
2025-12-04 15:35:00 +0100

    fix: improve download function and ensure data integrity with MD5 checks

--- bin/data/update.sh
+++ bin/data/update.sh
@@ -8,35 +8,44 @@ cd "$(dirname "$0")/../.."
 # Load environment variables.
 source .env
 
-# Ensure the directory for Versatiles data exists.
+# Ensure the directory for VersaTiles data exists.
 mkdir -p volumes/versatiles
 
 function download {
-    local URL="https://download.versatiles.org/${1}.versatiles"
-    local FILENAME="volumes/versatiles/${1}.versatiles"
+    local NAME="$1.versatiles"
+    local URL="https://download.versatiles.org/${NAME}"
 
-    mkdir -p volumes/temp
-    rm -rf volumes/temp/*
+    local MD5_LOCAL=$(cat "volumes/versatiles/${NAME}.md5" 2>/dev/null || echo "")
+    local MD5_REMOTE_URL=$(curl -s "${URL}.md5")
 
-    if [ ! -f "$FILENAME" ]; then
-        # Download OpenStreetMap data in Versatiles format.
+    if [ ! -f "volumes/versatiles/${NAME}" ] || [ "$MD5_LOCAL" != "$MD5_REMOTE_URL" ]; then
+        # Download OpenStreetMap data in VersaTiles format.
         if [ -z "$BBOX" ]; then
             # Download the complete dataset if BBOX is not specified.
             echo "Downloading the complete planet data..."
-            wget --progress=dot:giga "$URL" -O "volumes/temp/temp.versatiles"
+            wget "$URL" -O "volumes/temp/${NAME}"
         else
             # Download only the specified BBOX area.
             echo "Downloading data for specified bbox..."
-            docker run -v "$(pwd)/volumes/temp/:/data/:rw" versatiles/versatiles:latest-scratch versatiles convert --bbox "$BBOX" --bbox-border 3 "$URL" "/data/temp.versatiles"
+            docker run --rm -v "$(pwd)/volumes/temp/:/data/:rw" versatiles/versatiles:latest-scratch versatiles convert --bbox "$BBOX" --bbox-border 3 "$URL" "/data/${NAME}"
         fi
-        mv "volumes/temp/temp.versatiles" "$FILENAME"
+        wget "${URL}.md5" -O "volumes/temp/${NAME}.md5"
     fi
 }
 
+# Prepare a temporary directory for downloads.
+mkdir -p volumes/temp
+rm -rf volumes/temp/*
+
+# Download required datasets.
 download osm
 download hillshade-vectors
 download landcover-vectors
 download bathymetry-vectors
+
+# Move downloaded files to the final directory.
+mv volumes/temp/* volumes/versatiles/
+rm -rf volumes/temp
 
 # Check for successful download and setup.
 if [ ! -f volumes/versatiles/osm.versatiles ]; then
