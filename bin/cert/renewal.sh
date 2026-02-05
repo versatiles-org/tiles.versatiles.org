f581ea9e85e8de26c6c7be0f158c8ce8757c072a
Michael Kreil <github@michael-kreil.de>
2026-02-05 11:49:09 +0100

    fix: add strict mode and simplify error handling in renewal.sh

--- bin/cert/renewal.sh
+++ bin/cert/renewal.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 # This script renews SSL certificates using Certbot within a Docker environment,
 # then copies the new certificates to the nginx directory and reloads nginx.
@@ -11,23 +12,11 @@ source .env
 
 # Use Docker Compose to execute the Certbot 'renew' command in the 'certbot' service container
 docker compose run --rm certbot renew
-if [ $? -ne 0 ]; then
-   echo "Failed to renew certificates with Certbot."
-   exit 1
-fi
 
 # Copy the newly renewed certificates to the nginx directory
 cp -LfR ./volumes/certbot-cert/live/"${DOMAIN_NAME}" ./volumes/nginx-cert/live/
-if [ $? -ne 0 ]; then
-   echo "Failed to copy new certificates to nginx directory."
-   exit 1
-fi
 
 # Reload nginx to apply the new certificates
 docker compose exec nginx nginx -s reload
-if [ $? -ne 0 ]; then
-   echo "Failed to reload nginx. Please check the configuration."
-   exit 1
-fi
 
 echo "Certificate renewal and nginx reload completed successfully."
