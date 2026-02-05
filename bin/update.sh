7bdf2019a799da2539b05d10fabf8059cf8a92c1
Michael Kreil <github@michael-kreil.de>
2026-02-05 11:47:37 +0100

    fix: add strict mode and simplify error handling in update.sh

--- bin/update.sh
+++ bin/update.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 # This script performs a series of setup operations for a project:
 # - Pulls latest updates from Git
@@ -12,49 +13,25 @@ cd "$(dirname "$0")/.."
 # Update the repository with the latest changes from Git
 echo "Updating repository from Git..."
 git pull
-if [ $? -ne 0 ]; then
-    echo "Failed to pull updates from Git. Exiting."
-    exit 1
-fi
 
 # Update frontend
 echo "Fetching frontend..."
 ./bin/frontend/update.sh
-if [ $? -ne 0 ]; then
-    echo "Failed to update frontend. Exiting."
-    exit 1
-fi
 
 # Update data using a custom script
 echo "Fetching data..."
 ./bin/data/update.sh
-if [ $? -ne 0 ]; then
-    echo "Failed to update data. Exiting."
-    exit 1
-fi
 
 # test NGINX
 docker exec nginx sh -c "/docker-entrypoint.d/20-envsubst-on-templates.sh; nginx -t"
-if [ $? -ne 0 ]; then
-    echo "NGINX conf test failed. Exiting."
-    exit 1
-fi
 
 # Clear cache data using a custom script
 echo "Clearing cache data..."
 ./bin/ramdisk/clear.sh
-if [ $? -ne 0 ]; then
-    echo "Failed to clear cache data. Exiting."
-    exit 1
-fi
 
 # Restart Docker Compose services with force recreation to ensure a clean state
 echo "Restarting Docker Compose services..."
 docker compose pull
 docker compose up --detach --force-recreate --build
-if [ $? -ne 0 ]; then
-    echo "Failed to restart Docker Compose services. Exiting."
-    exit 1
-fi
 
 echo "Operations completed successfully."
