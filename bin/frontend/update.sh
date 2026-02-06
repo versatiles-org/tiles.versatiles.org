01142078b9c31f8664cd1fc8cbbb4e49ef34eb35
Michael Kreil <github@michael-kreil.de>
2026-02-06 17:58:03 +0100

    fix: update curl command in frontend update script to use lightweight HEAD request

--- bin/frontend/update.sh
+++ bin/frontend/update.sh
@@ -13,7 +13,7 @@ FRONTEND_FILE="volumes/versatiles/frontend.br.tar"
 TAG_FILE="volumes/versatiles/frontend.version"
 
 # Get the latest release tag via redirect (lightweight HEAD request, no body downloaded)
-LATEST_TAG=$(curl -fsSI -o /dev/null -w '%{url_effective}' https://github.com/versatiles-org/versatiles-frontend/releases/latest)
+LATEST_TAG=$(curl -fsSIL -o /dev/null -w '%{url_effective}' https://github.com/versatiles-org/versatiles-frontend/releases/latest)
 LATEST_TAG="${LATEST_TAG##*/}"
 
 # Check if we already have this version
