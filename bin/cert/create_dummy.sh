27a5bcc3671329283dab7c6acddd32a314d4d88a
Michael Kreil <github@michael-kreil.de>
2026-02-27 15:18:59 +0100

    fix: remove scheduled cron job for automatic certificate renewal from create_dummy.sh

--- bin/cert/create_dummy.sh
+++ bin/cert/create_dummy.sh
@@ -21,7 +21,3 @@ mkdir -p "./volumes/nginx-cert/live/${DOMAIN}/"
 openssl req -nodes -new -x509 -subj "/CN=${DOMAIN}" -keyout "./volumes/nginx-cert/live/${DOMAIN}/privkey.pem" -out "./volumes/nginx-cert/live/${DOMAIN}/fullchain.pem"
 
 echo "Dummy certificate created for ${DOMAIN}"
-
-# Schedule a weekly cron job for automatic certificate renewal (preserving existing jobs)
-CRON_JOB="23 5 * * 1 $(pwd)/bin/cert/renew.sh"
-( crontab -l 2>/dev/null | grep -v "bin/cert/renew.sh"; echo "$CRON_JOB" ) | crontab -
